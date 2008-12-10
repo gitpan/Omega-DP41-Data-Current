6e5e93f49b73f65cd035b959e00f94be74a576eb
Sam Kerr <kerr@cpan.org>
2008-12-10 20:33:52 +0000

    Import of KERR/Omega-DP41-Data-Current-0.01 from CPAN.
    gitpan-cpan-distribution: Omega-DP41-Data-Current
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KERR/Omega-DP41-Data-Current-0.01.tar.gz
    gitpan-cpan-author:       KERR
    gitpan-cpan-maturity:     released

---
+++ lib/Omega/DP41/Data/Current.pm
@@ -0,0 +1,107 @@
+package Omega::DP41::Data::Current;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Omega::DP41::Data::Current - The great new Omega::DP41::Data::Current!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Omega::DP41::Data::Current;
+
+    my $foo = Omega::DP41::Data::Current->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+S. Kerr, C<< <kerr at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-omega-dp41-data-current at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Omega-DP41-Data-Current>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Omega::DP41::Data::Current
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Omega-DP41-Data-Current>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Omega-DP41-Data-Current>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Omega-DP41-Data-Current>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Omega-DP41-Data-Current/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 S. Kerr, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Omega::DP41::Data::Current
